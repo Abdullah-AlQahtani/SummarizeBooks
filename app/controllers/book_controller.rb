b3b2ab0101bc738d94d1bd7d9320fc49dc556f13
triple0m <mrmohmmedmustafa@hotmail.com>
2018-11-29 12:00:24 +0300

    create Devise system & model and controller for Book

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,16 @@
+class BookController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get "pages/home"
+  # get "book/index"
+
+  root "pages#home"
+  # get 'book/new'
+  # get 'book/edit'
+  # get 'book/show'
+  # get "book/create"
 end
